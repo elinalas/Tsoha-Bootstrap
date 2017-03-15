7e2f57a6b25a4802b0716ae81b72443259dae381
elinalas <elina.lassila@helsinki.fi>
2017-03-15 09:32:30 +0200

    Sovellus alustettu ja versionhallinta käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME=”elinalas”
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER=”kilpailujarjestelma”
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
