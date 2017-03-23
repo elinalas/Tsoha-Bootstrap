73a9f272bf72ef2ef7d51220ff8a52e84ca72a16
elinalas <elina.lassila@helsinki.fi>
2017-03-23 17:29:26 +0200

    korjaus viikko 2, kaksi linkkiä puuttui

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,30 +1,30 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
-CREATE TABLE Kilpailu {
+CREATE TABLE Kilpailu (
  id SERIAL PRIMARY KEY,
  paivamaara INTEGER NOT NULL,
  nimi varchar(25) NOT NULL,
  taso varchar(10) NOT NULL,
  kilpailupaikka varchar(150) NOT NULL
-}
+)
 
-CREATE TABLE Kayttaja {
+CREATE TABLE Kayttaja (
  jasennumero INTEGER PRIMARY KEY NOT NULL,
  nimi varchar(150) NOT NULL,
  yllapitaja boolean DEFAULT FALSE
-}
+)
 
-CREATE TABLE Hevonen {
+CREATE TABLE Hevonen (
  rekisterinumero INTEGER PRIMARY KEY NOT NULL,
  nimi varchar(150) NOT NULL,
  kokoluokka varchar(10) NOT NULL,
  kayttaja INTEGER REFERENCES Kayttaja(jasennumero)
-}
+)
 
-CREATE TABLE Osallistuminen {
+CREATE TABLE Osallistuminen (
  id SERIAL PRIMARY KEY, 
  ratsastaja varchar(150) NOT NULL,
- ratsastajan_jasennumero varchar(150) NOT NULL,
+ ratsastajan_jasennumero integer NOT NULL,
  maksettu boolean DEFAULT FALSE,
  hevonen INTEGER REFERENCES Hevonen(rekisterinumero),
  kilpailu INTEGER REFERENCES Kilpailu(id)
-}
+)
